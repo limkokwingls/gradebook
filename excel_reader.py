1ce11b45dc248f27a220b56555da7b9720ed8261
Ntholi Nkhatho <ntholi@live.com>
2023-01-09 20:51:57 +0200

    create get_grades function

--- excel_reader.py
+++ excel_reader.py
@@ -1,10 +1,11 @@
 from pprint import pprint
+from types import NoneType
 import openpyxl
 from openpyxl.worksheet.worksheet import Worksheet
 
 
 def is_number(s):
-    if not s:
+    if not s or type(s) == NoneType:
         return False
     try:
         float(s)
@@ -13,24 +14,23 @@ def is_number(s):
     return True
 
 
-# Open the workbook
-wb = openpyxl.load_workbook("C:/Users/Temp/Desktop/test.xlsx")
+def get_grades():
+    wb = openpyxl.load_workbook("C:/Users/Temp/Desktop/test.xlsx")
 
+    sheet: Worksheet = wb['INTY1S1']  # type: ignore
 
-sheet: Worksheet = wb['INTY1S1']  # type: ignore
+    student_col = 'C'
+    marks_col = 'D'
 
-student_col = 'C'
-marks_col = 'D'
+    student_numbers = list([it.value for it in sheet[student_col]])
+    marks = list([it.value for it in sheet[marks_col]])
 
-student_numbers = list([it.value for it in sheet[student_col]])
-marks = list([it.value for it in sheet[marks_col]])
+    grades_book = list(zip(student_numbers, marks))
 
-grades_book = list(zip(student_numbers, marks))
+    result = []
 
-result = []
+    for grade in grades_book:
+        if is_number(grade[0]) and is_number(grade[1]):
+            result.append([int(float(grade[0])), int(grade[1])])
 
-for grade in grades_book:
-    if is_number(grade[0]) or True:
-        result.append([int(float(grade[0])), grade[1]])
-
-pprint(result)
+    return result
