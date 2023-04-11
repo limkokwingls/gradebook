a278272c147a28aaa9d1332a140dccb77a23e0b0
Ntholi Nkhatho <ntholi@live.com>
2023-04-11 15:34:27 +0200

    expand search for correct column letter

--- excel_reader.py
+++ excel_reader.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 from types import NoneType
-from openpyxl import Workbook
+
 import openpyxl
+from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from rich.prompt import Prompt
 from rich.console import Console
+from rich.prompt import Prompt
 
 from console_utils import print_in_table
 from model import CourseWork
 from utils import is_number
-
 
 error_console = Console(stderr=True, style="bold red")
 
@@ -19,8 +19,9 @@ def open_file() -> Workbook:
     while workbook == None:
         try:
             file_path = Prompt.ask(
-                "RAW Mark-sheet", default="C:\\Users\\Temp\\Desktop\\test.xlsx")
-            file_path = file_path.strip('\"')
+                "RAW Mark-sheet", default="C:\\Users\\Temp\\Desktop\\test.xlsx"
+            )
+            file_path = file_path.strip('"')
             if Path(file_path).is_file():
                 workbook = openpyxl.load_workbook(file_path)
         except Exception as e:
@@ -43,12 +44,20 @@ def read_numeric_column(sheet: Worksheet, col: str) -> list[str]:
 def find_marks_column(sheet: Worksheet, course_work: str):
     for col in sheet.iter_cols():
         for cell in col:
-            if type(cell.value) is str and cell.value.lower() == course_work.lower():
-                return cell.column_letter
+            if type(cell.value) is str:
+                value = " ".join(cell.value.lower().split())
+                if course_work.lower().endswith(value):
+                    return cell.column_letter
 
 
 def find_student_column(sheet: Worksheet):
     for col in sheet.iter_cols():
         for cell in col:
-            if type(cell.value) is str and cell.value.lower().startswith('student no'):
-                return cell.column_letter
+            if type(cell.value) is str:
+                value = cell.value.lower()
+                if (
+                    value == "student number"
+                    or value == "student id"
+                    or "student no" in value
+                ):
+                    return cell.column_letter
