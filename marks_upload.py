abc2e25cb1160cf1f9a3bbfeedc162f490af779a
Ntholi Nkhatho <ntholi@live.com>
2023-04-11 15:05:25 +0200

    display 'Marks' after course work name

--- marks_upload.py
+++ marks_upload.py
@@ -1,20 +1,24 @@
 import os
-from openpyxl import Workbook
-from rich.prompt import Prompt
-from pick import pick
-from browser import browser
 from pathlib import Path
+
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from pick import pick
+from rich import print
+from rich.console import Console
+from rich.prompt import Confirm, Prompt
+from rich.table import Table
+
+from browser import browser
 from console_utils import print_in_table
 from credentials import read_credentials, write_credentials
-from excel_reader import find_marks_column, find_student_column, open_file, read_numeric_column
-from openpyxl.worksheet.worksheet import Worksheet
+from excel_reader import (
+    find_marks_column,
+    find_student_column,
+    open_file,
+    read_numeric_column,
+)
 from model import CourseWork, Module
-from rich import print
-from rich.table import Table
-from rich.prompt import Confirm
-from rich.console import Console
-from rich.prompt import Prompt
-
 from pick_utils import EXIT_LABEL, multiple_pick
 from utils import is_number
 
@@ -23,7 +27,7 @@ error_console = Console(stderr=True, style="bold red")
 
 
 def get_marks_for(grades: list[dict[str, str]], student_number: list[tuple[str]]):
-    marks = ''
+    marks = ""
     for grade in grades:
         key, value = next(iter(grade.items()))
         if key == student_number[0]:
@@ -38,7 +42,9 @@ def pick_worksheet(workbook: Workbook) -> Worksheet | None:
     options = [str(it) for it in list]
     options.append(EXIT_LABEL)
     _, index = pick(
-        options, f"Pick a Sheet", indicator='->',
+        options,
+        f"Pick a Sheet",
+        indicator="->",
     )
     if index <= (len(list) - 1):  # type: ignore
         sheet_name = list[index]  # type: ignore
@@ -55,8 +61,7 @@ def pick_course_works(course_works: list) -> list[CourseWork] | None:
 def get_workbook_std_numbers(sheet: Worksheet):
     col = find_student_column(sheet)
     while True:
-        col = Prompt.ask(
-            f"- Student Numbers", default=col)
+        col = Prompt.ask(f"- Student Numbers", default=col)
         if col and col.isalpha():
             break
         error_console.print("Should be an alphabet")
@@ -69,7 +74,7 @@ def get_workbook_marks(course_works: list[CourseWork], sheet: Worksheet):
     for cw in course_works:
         while True:
             col = Prompt.ask(
-                f"- {cw.fullname()}",
+                f"- {cw.fullname()} Marks",
                 default=find_marks_column(sheet, cw.fullname()),
             )
             if col and col.isalpha():
@@ -111,8 +116,9 @@ def get_course_work_fullname(cw_id: str, course_works: list[CourseWork]):
     return cw_id
 
 
-def confirm_gradebook(gradebook: dict[str, dict[str, str]],
-                      course_works: list[CourseWork]):
+def confirm_gradebook(
+    gradebook: dict[str, dict[str, str]], course_works: list[CourseWork]
+):
     data = {}
     for i, key in enumerate(gradebook.keys()):
         cw = get_course_work_fullname(key, course_works)
@@ -125,10 +131,11 @@ def confirm_gradebook(gradebook: dict[str, dict[str, str]],
     return Confirm.ask("Proceed?", default=True)
 
 
-def create_payloads(course_work: CourseWork,
-                    gradebook: dict[str, dict[str, str]],
-                    cms_std_numbers: dict[str, str]):
-
+def create_payloads(
+    course_work: CourseWork,
+    gradebook: dict[str, dict[str, str]],
+    cms_std_numbers: dict[str, str],
+):
     course_work_res = gradebook[course_work.id]
     marks = []
     ids = []
@@ -137,10 +144,7 @@ def create_payloads(course_work: CourseWork,
             ids.append(cms_std_numbers[std])
             marks.append(course_work_res[std])
 
-    return {
-        "x_StdModuleID[]": ids,
-        f'x_{course_work.id}[]': marks
-    }
+    return {"x_StdModuleID[]": ids, f"x_{course_work.id}[]": marks}
 
 
 def main(module: Module):
@@ -152,14 +156,16 @@ def main(module: Module):
         cms_std_id, course_works = browser.read_cms_gradebook(module)
         if not course_works:
             error_console.print(
-                f"{str(module)} does not have any assessments set, please set assessments first in the CMS")
+                f"{str(module)} does not have any assessments set, please set assessments first in the CMS"
+            )
             break
         while True:
             course_works = pick_course_works(course_works)  # type: ignore
             if not course_works:
                 continue
             print(
-                f"\nReady to read from {sheet.title}, enter column letters for the following:")
+                f"\nReady to read from {sheet.title}, enter column letters for the following:"
+            )
             student_numbers = get_workbook_std_numbers(sheet)
             marks = get_workbook_marks(course_works, sheet)
             gradebook = create_gradebook(student_numbers, marks)
